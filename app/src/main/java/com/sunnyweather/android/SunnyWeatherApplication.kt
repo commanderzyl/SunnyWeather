99ab7f5742529fac5139fa7a72cd14ef89df3ca7
yingli zhang <yingli.zhang@samsung.com>
2021-07-20 16:29:32 +0800

    实现搜索全球城市数据功能。

---
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
@@ -0,0 +1,18 @@
+package com.sunnyweather.android
+
+import android.app.Application
+import android.content.Context
+
+class SunnyWeatherApplication: Application() {
+
+    companion object {
+        @SuppressWarnings("StaticFieldLeak")
+        lateinit var context: Context
+        const val TOKEN = "rd2RjYKL2Irhp46V"
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+}
